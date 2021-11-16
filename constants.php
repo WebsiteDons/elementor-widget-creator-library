b668b450d6cadb0ef4c455511be17ce9587026d3
WebsiteDons <42153624+WebsiteDons@users.noreply.github.com>
2021-11-15 22:32:03 -0500

    Update constants.php

--- constants.php
+++ constants.php
@@ -13,33 +13,38 @@ global $cmse;
 $cmse = function($query)
 {
 	$const = (object)[
-	// set the prefered prefix which displays on the widget button
-	// can be empty. ideal if widgets will be mixed in with another category
+	// set the prefered prefix which displays on the widget button 
 	// and title bar of the widget when in edit view
 	'widgetprefix'	=>	'CMSE',
+	/* 
+	keywords are used to find widgets via Elementor's editor search
+	 separate each word by comma
+	 set keywords in each widget's xml and separate each word by comma
+	 example: <widget icon="eicon-edit" cat="basic" keywords="slideshow,kenburns,woocommerce">
+	 a setting here is purely for default if preferred
+	*/
+	'keywords'		=> '',
 	
-	// widgets panel section ID, icon and name
-	'panelid'	=>	'cmse-widgetcat',
-	'panelcat'	=>	'CMSE Widgets',
-	'panelicon'	=>	'fa fa-plug', // a default icon
+	// widgets panel section ID, icon and name. 
+	// Icon is default applied to each widget in case none is set via the xml file
+	'panelid'		=>	'cmse-widgetcat',
+	'panelcat'		=>	'CMSE Widgets',
+	'panelicon'		=>	'fa fa-plug',
 	
 	// text domain for the language ID
 	'textdomain'	=>	'plugin-name',
 	
 	// paths
-	'widgetpath'	=>	WP_CONTENT_DIR.'/elementor-widgets/widgets/',
-	// optional to locate any custom controls 
-	'customcontrol'	=>	WP_CONTENT_DIR.'/elementor-widgets/controls', 
+	'widgetpath'	=>	WP_PLUGIN_DIR.'/cmse-elementor-lib/widgets/',
+	'customcontrol'	=>	WP_PLUGIN_DIR.'/cmse-elementor-lib/includes/controls',
 	
 	// if custom separator shapes are in use set path to elementor's separator folder
 	// and the path to your source where the shapes are stored
-	'copydir'	=>	WP_CONTENT_DIR.'/elementor-widgets/svg',
-	'shapesdir'	=>	WP_PLUGIN_DIR.'/elementor/assets/shapes',
-	
-	// not a requirement but will serve to style or adjust widget elements in the editor
-	'cssfileurl'	=>	get_bloginfo('url').'/wp-content/elementor-widgets/elementor-alt.css',
-	'cssid'		=>	'elementor-alt'
+	'copydir'		=>	WP_PLUGIN_DIR.'/cmse-elementor-lib/assets/svg',
+	'shapesdir'		=>	WP_PLUGIN_DIR.'/elementor/assets/shapes',
+	'cssfileurl'	=>	get_bloginfo('url').'/wp-content/plugins/cmse-elementor-lib/assets/elementor-editor.css',
+	'cssid'			=>	'cmse-elementor-editor'
 	];
 	
 	return $const->$query;
-}; 
+};
